a2ceee8e66a26e957029492efba4f56b5bc68bab
Adole Samuel Sani <adolesamuel4@gmail.com>
2023-02-06 20:48:05 +0100

    tips widget created

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:faramove_therapy/constants/asset_strings.dart';
 import 'package:faramove_therapy/screens/home/home_page_appbar.dart';
 import 'package:faramove_therapy/screens/home/widgets/quick_actions_card.dart';
+import 'package:faramove_therapy/screens/home/widgets/tips_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
@@ -87,77 +88,13 @@ class _HomePageState extends State<HomePage> {
             height: 6.0.h,
             color: const Color(0xFFF8F9FB),
           ),
+          //
           TipsCard(
             title: 'Tips to stay healthy',
             subtitle: 'Get simple health tips.',
             onTap: () {},
           ),
         ],
-      ),
-    );
-  }
-}
-
-class TipsCard extends StatelessWidget {
-  final String title;
-  final String? subtitle;
-  final Function()? onTap;
-  const TipsCard({
-    super.key,
-    required this.title,
-    this.subtitle,
-    this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 56.0.h,
-      child: Material(
-        child: InkWell(
-          onTap: onTap,
-          child: SizedBox(
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 10.0.h),
-              child: Row(
-                children: [
-                  Gap(20.0.w),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        title,
-                        style: const TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      if (subtitle != null) ...[
-                        Gap(3.0.h),
-                        Text(
-                          subtitle!,
-                          style: const TextStyle(
-                            color: Color(0xFF888D95),
-                            fontSize: 12.0,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        )
-                      ]
-                    ],
-                  ),
-                  const Spacer(),
-                  Image.asset(
-                    AssetStrings.dashArrow,
-                    height: 16.0,
-                    width: 26.43,
-                  ),
-                  Gap(20.0.w),
-                ],
-              ),
-            ),
-          ),
-        ),
       ),
     );
   }
