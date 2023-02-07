27b262dc9381e798bee4102305f82c3127877be1
Adole Samuel Sani <adolesamuel4@gmail.com>
2023-02-07 16:44:45 +0100

    Update podcast_page.dart

--- lib/screens/podcast/podcast_page.dart
+++ lib/screens/podcast/podcast_page.dart
@@ -80,35 +80,81 @@ class _PodCastPageState extends State<PodCastPage> {
                 initialChildSize: 0.1,
                 builder:
                     (BuildContext context, ScrollController scrollController) {
-                  return Container(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 14.0, horizontal: 12.0),
-                    decoration: const BoxDecoration(
-                      color: Color(0xFF3079EC),
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(6.0),
-                        topRight: Radius.circular(6.0),
-                      ),
-                    ),
-                    child: SingleChildScrollView(
-                      controller: scrollController,
-                      child: SizedBox(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            //Controls
-                            SizedBox(
-                              height: 100,
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const Padding(
-                                    padding:
-                                        EdgeInsets.symmetric(vertical: 8.0),
+                  //So some new layout can be built when constraints changes.
+                  //lowkey just using it to setstate and switch shrink to expand.
+                  return LayoutBuilder(
+                    builder: (context, constraints) {
+                      return Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 14.0, horizontal: 12.0),
+                        decoration: const BoxDecoration(
+                          color: Color(0xFF3079EC),
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(6.0),
+                            topRight: Radius.circular(6.0),
+                          ),
+                        ),
+                        child: SingleChildScrollView(
+                          controller: scrollController,
+                          child: SizedBox(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                //Controls
+                                SizedBox(
+                                  height: 100,
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      const Padding(
+                                        padding:
+                                            EdgeInsets.symmetric(vertical: 8.0),
+                                        child: Text(
+                                          'Transcript',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 14.0,
+                                          ),
+                                        ),
+                                      ),
+                                      ExpandButton(
+                                        text: !dragController.isAttached
+                                            ? 'EXPAND'
+                                            : dragController.size > 0.1
+                                                ? 'SHRINK'
+                                                : 'EXPAND',
+                                        onTap: () {
+                                          if (dragController.size < 0.3) {
+                                            dragController.animateTo(0.4,
+                                                duration: const Duration(
+                                                    milliseconds: 300),
+                                                curve: Curves.easeInOut);
+                                          } else {
+                                            dragController.animateTo(0.0,
+                                                duration: const Duration(
+                                                    milliseconds: 300),
+                                                curve: Curves.easeInOut);
+                                          }
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+
+                                //Lyrics sheets
+                                const Align(
+                                  alignment: Alignment.topCenter,
+                                  child: SizedBox(
                                     child: Text(
-                                      'Transcript',
+                                      'The Sweet Chariot that takes you to valhalla is at the end of everything glorious\n\n'
+                                      'May your focus ever remain as sharp as an Axe drenched in the sweat of blacksmiths from Kattegatt\n\n'
+                                      'May your horse ride Bold and True\n\n'
+                                      'May your eyes stay sharp\n\n'
+                                      'May Odin welcome you with Joy for you have been Victorious\n\n',
                                       style: TextStyle(
                                         color: Colors.white,
                                         fontWeight: FontWeight.w600,
@@ -116,52 +162,13 @@ class _PodCastPageState extends State<PodCastPage> {
                                       ),
                                     ),
                                   ),
-                                  ExpandButton(
-                                    text: !dragController.isAttached
-                                        ? 'EXPAND'
-                                        : dragController.size > 0.1
-                                            ? 'SHRINK'
-                                            : 'EXPAND',
-                                    onTap: () {
-                                      if (dragController.size < 0.3) {
-                                        dragController.animateTo(0.4,
-                                            duration: const Duration(
-                                                milliseconds: 300),
-                                            curve: Curves.easeInOut);
-                                      } else {
-                                        dragController.animateTo(0.0,
-                                            duration: const Duration(
-                                                milliseconds: 300),
-                                            curve: Curves.easeInOut);
-                                      }
-                                    },
-                                  ),
-                                ],
-                              ),
-                            ),
-
-                            //Lyrics sheets
-                            const Align(
-                              alignment: Alignment.topCenter,
-                              child: SizedBox(
-                                child: Text(
-                                  'The Sweet Chariot that takes you to valhalla is at the end of everything glorious\n\n'
-                                  'May your focus ever remain as sharp as an Axe drenched in the sweat of blacksmiths from Kattegatt\n\n'
-                                  'May your horse ride Bold and True\n\n'
-                                  'May your eyes stay sharp\n\n'
-                                  'May Odin welcome you with Joy for you have been Victorious\n\n',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: 14.0,
-                                  ),
                                 ),
-                              ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ),
+                      );
+                    },
                   );
                 },
               ),
